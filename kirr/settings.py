4c86f2a8a5ac27619ece053f5b58ff71e65160c2
User <user@Users-MacBook-Pro.local>
2018-12-30 03:26:09 +0300

    .

--- kirr/settings.py
+++ kirr/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
-#
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static', 'static_dirs'),
 #
